030ec4c0eace7f09fe190992b540a70d4962416f
saqib.javed <info@it-blue.com>
2025-11-25 10:29:22 +0100

    Remove CheckNoError and E2E Test tools; add Gengora specification document

--- gengora/server/Models/GeneratorOutputParams.cs
+++
@@ -1,9 +0,0 @@
-namespace BITS.Gengora.Server.Models;
-
-/// <summary>
-/// Parameters for generator stdout/stderr notifications.
-/// </summary>
-public class GeneratorOutputParams
-{
-    public string Text { get; set; } = string.Empty;
-}
